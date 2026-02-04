0bca1c01ab1957096bbf0b35546214db3d1718f2
FLACKO <kaiscott.centura@gmail.com>
2026-02-04 18:27:59 +0530

    fixing some stuff (1)

--- functions/bournefish.fish
+++ functions/bournefish.fish
@@ -14,18 +14,25 @@ function bournefish --description "Bash compatibility layer for Fish"
 
     set -l bash_detected 0
 
+    # 1. Inline variable assignment: VAR=val ...
     if string match -qr '^\s*[A-Za-z_][A-Za-z0-9_]*=' "$trimmed_cmd"
         set bash_detected 1
-    else if string match -qr '`.*`' "$trimmed_cmd"
+    # 2. Backticks (anywhere)
+    else if string match -qr '`' "$trimmed_cmd"
         set bash_detected 1
+    # 3. Bash subshells $(...) or double dynamic variables ${...}
     else if string match -qr '\$\(.*\)' "$trimmed_cmd"
         set bash_detected 1
+    # 4. Double brackets [[ ... ]]
     else if string match -qr '\[\[ .* \]\]' "$trimmed_cmd"
         set bash_detected 1
-    else if string match -qr '&>' "$trimmed_cmd"
+    # 5. Bash redirections
+    else if string match -qr '(&>|2>&1|1>&2)' "$trimmed_cmd"
         set bash_detected 1
+    # 6. Test brackets [ ... ] at the start
     else if string match -qr '^\s*\[\s+' "$trimmed_cmd"
         set bash_detected 1
+    # 7. Bash builtins
     else if string match -qr '^\s*(export|source|shopt|local|declare|typeset)\s+' "$trimmed_cmd"
         set bash_detected 1
     end
